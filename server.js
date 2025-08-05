863ef4f1266fa4ed0b57e8d13b1178b91cf06ba5
sanketkalekar <kalekarsanket005@gmail.com>
2025-08-06 00:01:07 +0530

    Added missing config

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const limiter = rateLimit({
 
 // Middleware
 app.use(cors({
-  origin: [process.env.FRONTEND_URL ||  'http://localhost:5173'],
+  origin: [process.env.FRONTEND_URL ||  'https://me-main-front.vercel.app/'],
   credentials: true
 }));
 app.use(express.json());
